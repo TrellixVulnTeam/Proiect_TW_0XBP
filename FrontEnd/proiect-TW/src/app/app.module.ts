3bffc954acbc370cf1a289d3c4dad3a6c2e45239
DaianaBoczki <daiana.boczki00@e-uvt.ro>
2021-11-10 10:09:52 +0200

    ng-bootstrap installed

--- FrontEnd/proiect-TW/src/app/app.module.ts
+++ FrontEnd/proiect-TW/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
